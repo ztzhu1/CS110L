91630d22c670fa4246dea0574f0609a1e7373598
ztzhu <zhuzitian@zju.edu.cn>
2023-01-09 17:34:29 +0800

    Finish wc

--- week2/rwc/src/main.rs
+++ week2/rwc/src/main.rs
@@ -1,5 +1,7 @@
 use std::env;
 use std::process;
+use std::fs::File;
+use std::io::{self, BufRead};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,5 +10,18 @@ fn main() {
         process::exit(1);
     }
     let filename = &args[1];
+
     // Your code here :)
+    let file = File::open(filename).expect("Fail to open file!");
+    let mut s: u32 = 0;
+
+    for line in io::BufReader::new(file).lines() {
+        let line_str = line.unwrap();
+        for word in line_str.split(" ") {
+            if word.len() > 0 {
+                s += 1;
+            }
+        }
+    }
+    println!("{} has {} words.", filename, s);
 }
